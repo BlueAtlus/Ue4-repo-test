48d202fb026f3d820f7d25c0e5b0897f40fc2b6a
BlueAtlus <ali_zayadi@hotmail.com>
2017-09-15 18:04:34 -0400

    Added: All Test ue4 project Files

---
+++ Source/SourceControl_Test/SourceControl_Test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SourceControl_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SourceControl_Test, "SourceControl_Test" );
